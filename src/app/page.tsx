fde607f19e7c0c39463d21ad105f30eb9d44450b
Mirado <vonjiniainamirado@gmail.com>
2026-06-21 18:40:26 +0300

    Initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="flex min-h-dvh items-center justify-center">
+      <Button>Hello Mémoire</Button>
+    </div>
+  );
+};
+
+export default App;
